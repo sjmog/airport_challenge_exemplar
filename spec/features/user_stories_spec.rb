2ecade05e62b67f0d4511191f14f43a4602e0df0
Sam Morgan <s_morgan@me.com>
2015-10-20 12:21:07 +0100

    User Story 4
    As an air traffic controller
    So that I can avoid accidents
    I want to prevent airplanes landing or taking off when the weather is stormy

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -44,5 +44,9 @@ describe 'User Stories' do
     it 'does not allow planes to land' do
       expect { airport.land(plane) }.to raise_error 'Cannot land plane: weather is stormy'
     end
+
+    it 'does not allow planes to take off' do
+      expect { airport.take_off(plane) }.to raise_error 'Cannot take off plane: weather is stormy'
+    end
   end
 end
