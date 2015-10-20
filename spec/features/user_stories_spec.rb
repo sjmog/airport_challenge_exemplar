9da67659f06a9ea932427d83ca252081ff38211d
Sam Morgan <s_morgan@me.com>
2015-10-20 11:26:36 +0100

    User Story 1
    As an air traffic controller
    So planes can land safely at my airport
    I would like to instruct a plane to land

---
+++ spec/features/user_stories_spec.rb
@@ -0,0 +1,10 @@
+describe 'User Stories' do
+  # As an air traffic controller
+  # So planes can land safely at my airport
+  # I would like to instruct a plane to land
+  it 'so planes land at airports, instruct a plane to land' do
+    airport = Airport.new
+    plane = Plane.new
+    expect { airport.land(plane) }.not_to raise_error
+  end
+end
