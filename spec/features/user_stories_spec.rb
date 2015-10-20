11f5cf9bdc7f1bd421aea3027a202bff8b6113a8
Sam Morgan <s_morgan@me.com>
2015-10-20 13:38:26 +0100

    User Story 9
    As an air traffic controller
    So the system is consistent and correctly reports plane status and location
    I want to ensure a plane that has taken off from an airport is no longer in that airport

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -68,6 +68,15 @@ describe 'User Stories' do
     end
 
     # As an air traffic controller
+    # So the system is consistent and correctly reports plane status and location
+    # I want to ensure a plane that has taken off from an airport is no longer in that airport
+    it 'taking off a plane removes it from that airport' do
+      airport.land(plane)
+      airport.take_off(plane)
+      expect(airport.planes).not_to include plane
+    end
+
+    # As an air traffic controller
     # So that I can avoid collisions
     # I want to prevent airplanes landing when my airport is full
     context 'when airport is full' do
